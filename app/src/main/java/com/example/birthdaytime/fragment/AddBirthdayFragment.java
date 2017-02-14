5129ff850217b0388c8fdae5eeb68973beb5587c
admin <gz.ashish@gmail.com>
2017-02-14 15:04:06 +0545

    adding to git hub

--- app/src/main/java/com/example/birthdaytime/fragment/AddBirthdayFragment.java
+++ app/src/main/java/com/example/birthdaytime/fragment/AddBirthdayFragment.java
@@ -1,7 +1,5 @@
 package com.example.birthdaytime.fragment;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,6 +18,8 @@ import com.example.birthdaytime.birthdayInfo;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * Created by admin on 2/2/2017.
@@ -85,24 +85,48 @@ public class AddBirthdayFragment extends Fragment {
     }
 
     public void alertDialouge(final String id) {
-        // Log.i("delect----","alertDialouge: ");
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Delect Birthday");
-        builder.setMessage("Are you Sure!!!");
-        builder.setPositiveButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+        new SweetAlertDialog(getActivity(), SweetAlertDialog.WARNING_TYPE)
+                .setTitleText("Are you sure?")
+                .setContentText("Won't be able to recover this file!")
+                .setCancelText("No,cancel plz!")
+                .setConfirmText("Yes,delete it!")
+                .showCancelButton(true)
+                .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        // reuse previous dialog instance, keep widget user state, reset them if you need
+                        sDialog.setTitleText("Cancelled!")
+                                .setContentText("Your Birthday is safe :)")
+                                .setConfirmText("OK")
+                                .showCancelButton(false)
+                                .setCancelClickListener(null)
+                                .setConfirmClickListener(null)
+                                .changeAlertType(SweetAlertDialog.ERROR_TYPE);
 
-            }
-        });
-        builder.setNegativeButton("YES", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                databaseHelper.delectBirthday(id);
-                displayBirthdayList();
-            }
-        });
-        builder.show();
+                        // or you can new a SweetAlertDialog to show
+                               /* sDialog.dismiss();
+                                new SweetAlertDialog(SampleActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                        .setTitleText("Cancelled!")
+                                        .setContentText("Your imaginary file is safe :)")
+                                        .setConfirmText("OK")
+                                        .show();*/
+                    }
+                })
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        sDialog.setTitleText("Deleted!")
+                                .setContentText("Your BIrthday has been deleted!")
+                                .setConfirmText("OK")
+                                .showCancelButton(false)
+                                .setCancelClickListener(null)
+                                .setConfirmClickListener(null)
+                                .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                        databaseHelper.delectBirthday(id);
+                        displayBirthdayList();
+                    }
+                })
+                .show();
     }
 
     public String findAge(String giveYear, String givenMonth, String givenDay) {
